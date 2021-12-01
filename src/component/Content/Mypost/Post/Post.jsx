3a9f171dbd7a25264d34188aeb1cc094b6905e43
Vladislav Sotnikov <OSWVSotnikov@oriflame.com>
2021-12-01 18:33:51 +0100

    Change name

--- src/component/Content/Mypost/Post/Post.jsx
+++ src/component/Content/Mypost/Post/Post.jsx
@@ -9,7 +9,7 @@ const Post = (props) => {
       <img src='https://seeding.com.ua/wp-content/uploads/2017/04/%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D0%B0-%D0%B4%D0%BB%D1%8F-%D0%BE%D1%82%D0%B7%D1%8B%D0%B2%D0%BE%D0%B2.jpg'></img>
         {props.massege}
         <div>
-        <span>like</span> {props.like}
+        <span>likeeeeee</span> {props.like}
         </div>
         </div>
         );
