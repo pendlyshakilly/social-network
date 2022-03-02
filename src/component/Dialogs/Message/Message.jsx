0006cf1991e2b603a79cabf7d2d13053b8fe0565
Illy <pendlyshakilly@gmail.com>
2022-03-02 16:25:45 +0200

    lesson 26

---
+++ src/component/Dialogs/Message/Message.jsx
@@ -0,0 +1,9 @@
+import { NavLink } from 'react-router-dom';
+import s from './../Dialogs.module.css'
+
+
+const Message = (props) => {
+    return <div className={s.dialog}>{props.message}</div>
+}
+
+export default Message;
