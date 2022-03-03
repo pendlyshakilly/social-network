3e7e33720a49a84c708a6e5dc3403d76d37af1c6
Vladislav Sotnikov <OSWVSotnikov@oriflame.com>
2022-03-03 21:47:22 +0100

    Pretty fix

---
+++ src/component/Dialogs/Dialogs.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import s from './Dialogs.module.css'
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
+
+const Dialogs = (props) => {
+    let messageElement = props.messages.map(m => <Message message={m.message}/>)
+ 
+    let dialogElement = props.dialog.map(d => <DialogItem name={d.name} id={d.id}/> )
+
+    return (
+            <div className={s.dialogs}> 
+                <div className={s.dialogItem}>
+                {dialogElement}
+                </div>
+                <div className={s.messages}>
+            {messageElement}  
+                </div>
+            </div>
+        );
+}
+
+export default Dialogs;
